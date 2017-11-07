87770eac59be29395324ddfda539c1a1099e8d0f
thWinterSun <2861492134@qq.com>
2017-11-07 15:30:33 +0800

    Transfer the main body of the project from menu.vue to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,63 @@
 <template>
   <div id="app">
-    <template>
-  
-  </template>
-
+    <!--导航组件  -->
+    <top-menus></top-menus>
+    <!--主体内容  -->
+    <div class="layout-content">
+      <div class="layout-breadcrumb">
+        <Breadcrumb>
+          <BreadcrumbItem href="/">Home</BreadcrumbItem>
+          <BreadcrumbItem href="/home/runstatus">runstatus</BreadcrumbItem>
+          <BreadcrumbItem>Breadcrumb</BreadcrumbItem>
+        </Breadcrumb>
+      </div>
+      <router-view></router-view>
+    </div>
+    <!--页脚  -->
+    <div class="layout-copy">
+        2017 &copy; philisense
+    </div>
   </div>
 </template>
 
 <script>
+import topMenus from './components/menu.vue'
 export default {
+  name: 'top-menu',
   data () {
     return {
-      
     }
-  }
+  },
+  components: { topMenus }
 }
 </script>
 
-<style>
-
+<style scoped>
+/**/
+.layout-assistant{
+    width: 300px;
+    margin: 0 auto;
+    height: inherit;
+}
+.layout-breadcrumb{
+    padding: 10px 15px 0;
+}
+.layout-content{
+    width: 100%;
+    max-width: 940px;
+    margin-left: auto;
+    margin-right: auto;
+    zoom: 1;
+    min-height: 830px;
+    background: #ddd;
+    border-radius: 4px;
+}
+.layout-content-main{
+    padding: 10px;
+}
+.layout-copy{
+    text-align: center;
+    padding: 10px 0 20px;
+    color: #9ea7b4;
+}
 </style>
--- src/components/menu.vue
+++ src/components/menu.vue
@@ -36,34 +36,9 @@
       margin:0;
       background-color:#495060;
     }    
-    /**/
-    .layout-assistant{
-        width: 300px;
-        margin: 0 auto;
-        height: inherit;
-    }
   
-    .layout-breadcrumb{
-        padding: 10px 15px 0;
-    }
-    .layout-content{
-        width: 100%;
-        max-width: 940px;
-        margin-left: auto;
-        margin-right: auto;
-        zoom: 1;
-        min-height: 830px;
-        background: #ddd;
-        border-radius: 4px;
-    }
-    .layout-content-main{
-        padding: 10px;
-    }
-    .layout-copy{
-        text-align: center;
-        padding: 10px 0 20px;
-        color: #9ea7b4;
-    }
+  
+    
 </style>
 <template>
     <div class="layout">
@@ -84,19 +59,7 @@
             </div>
         </Menu>
         
-        <div class="layout-content">
-          <div class="layout-breadcrumb">
-            <Breadcrumb>
-              <BreadcrumbItem href="/">Home</BreadcrumbItem>
-              <BreadcrumbItem href="/home/runstatus">runstatus</BreadcrumbItem>
-              <BreadcrumbItem>Breadcrumb</BreadcrumbItem>
-            </Breadcrumb>
-          </div>
-          <router-view></router-view>
-        </div>
-        <div class="layout-copy">
-            2017 &copy; philisense
-        </div>
+        
     </div>
 </template>
 <script>
